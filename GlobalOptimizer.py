1b22e97ff3265cb9e16453aa5ba5614430e2ec73
aitorbilbao <aitorbilbao25@gmail.com>
2024-11-19 16:30:42 +0100

    Added Setup

--- GlobalOptimizer.py
+++ GlobalOptimizer.py
@@ -1,13 +1,18 @@
 from abc import ABC, abstractmethod
+from ASE import Atoms
 import numpy as np
 
 class GlobalOptimizer(ABC):
 
-    def __init__(self, clusters, localOptimizer):
+    def __init__(self, clusters: int, localOptimizer, atoms: int):
         self.history = np.array([])
         self.clusters = clusters
+        self.clusterList = []
         self.localOptimizer = localOptimizer
         self.currentIteration = 0
+        self.atoms = atoms
+        self.covalentRadius = 1.0
+        self.boxLength = 2*self.covalentRadius * (1/2 + ((3*self.atoms)/(4*np.pi*np.sqrt(2)))**(1/3))
         # Disturber
 
     @abstractmethod
@@ -20,6 +25,11 @@ class GlobalOptimizer(ABC):
 
     @abstractmethod
     def setup(self):
+        for cluster in self.clusters:
+            config = ( np.random.rand(self.atoms, 3) - 0.5 ) *  self.boxLength * 1.5 #1.5 is a magic number
+            #In the future, instead of number of atoms, we ask the user to choose how many atoms they want for each atom type.
+            clus = Atoms('LJ' + str(self.atoms), positions=config)
+            self.clusterList.append(clus)   
         pass
 
     def run(self, maxIterations):
