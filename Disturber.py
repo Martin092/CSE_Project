d9bf53814c4077fd768f369f5ce2ef06789b32e3
Mohammed Balfakeih <M.H.M.Balfakeih@student.tudelft.nl>
2024-11-19 15:33:58 +0100

    Implemented Molecular Dynamics in Disturber

--- Disturber.py
+++ Disturber.py
@@ -1,3 +1,7 @@
+from ase.units import fs
+from ase.md.langevin import Langevin
+
+
 class Disturber():
 
     # Class with all the methods to disturb a cluster
@@ -11,8 +15,21 @@ class Disturber():
     def angular_movement(self, cluster):
         pass
 
-    def MD(self, cluster):
-        pass
+    def MD(self, cluster, temperature, number_of_steps):
+        """
+        Perform a Molecular Dynamics run using Langevin Dynamics
+        :param cluster: Cluster of atoms
+        :param temperature: Temperature in Kelvin
+        :param number_of_steps: Number of steps to use in Molecular Dynamics
+        """
+        dyn = Langevin(
+            cluster,
+            timestep=5.0 * fs, # Feel free to mess with this parameter
+            temperature_K=temperature,
+            friction=0.5 / fs,  # Feel free to mess with this parameter
+        )
+
+        dyn.run(number_of_steps)
 
     def twist(self, cluster):
         pass
