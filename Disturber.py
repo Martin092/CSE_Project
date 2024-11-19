a100045f860e240e789f24eb7c4ca2615d47b9c8
Mohammed Balfakeih <M.H.M.Balfakeih@student.tudelft.nl>
2024-11-19 15:17:22 +0100

    Boilerplate for disturber class

---
+++ Disturber.py
@@ -0,0 +1,21 @@
+class Disturber():
+
+    # Class with all the methods to disturb a cluster
+
+    def __init__(self, local_optimizer):
+        self.local_optimizer = local_optimizer
+
+    def random_setup(self, cluster):
+        pass
+
+    def angular_movement(self, cluster):
+        pass
+
+    def MD(self, cluster):
+        pass
+
+    def twist(self, cluster):
+        pass
+
+    def etching(self, cluster):
+        pass
