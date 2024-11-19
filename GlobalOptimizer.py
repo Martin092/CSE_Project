0de105a6e5cb51dd99276a4e9f183f376aa57027
Martin <martin.damianof@gmail.com>
2024-11-19 15:05:00 +0100

    abstract methods

--- GlobalOptimizer.py
+++ GlobalOptimizer.py
@@ -2,9 +2,28 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 class GlobalOptimizer(ABC):
+
     def __init__(self, clusters, localOptimizer):
         self.history = np.array([])
         self.clusters = clusters
         self.localOptimizer = localOptimizer
         self.currentIteration = 0
         # Disturber
+
+    @abstractmethod
+    def iteration(self):
+        pass
+
+    @abstractmethod
+    def isConverged(self):
+        pass
+
+    @abstractmethod
+    def setup(self):
+        pass
+
+    def run(self, maxIterations):
+        self.setup()
+
+        while self.currentIteration < maxIterations or not self.isConverged():
+            self.iteration()
