108a5c1833bdf17b256fae18869c5217991486b2
Kaloyan Yanchev <k.yanchev-1@student.tudelft.nl>
2024-11-19 16:13:37 +0100

    Cluster splitting

--- Disturber.py
+++ Disturber.py
@@ -1,4 +1,7 @@
-class Disturber():
+import numpy as np
+
+
+class Disturber:
 
     # Class with all the methods to disturb a cluster
 
@@ -11,7 +14,7 @@ class Disturber():
     def angular_movement(self, cluster):
         pass
 
-    def MD(self, cluster):
+    def md(self, cluster):
         pass
 
     def twist(self, cluster):
@@ -19,3 +22,19 @@ class Disturber():
 
     def etching(self, cluster):
         pass
+
+    @staticmethod
+    def split_cluster(cluster, p1=np.random.rand(3), p2=np.random.rand(3), p3=np.random.rand(3)):
+        v1 = p2 - p1
+        v2 = p3 - p1
+        normal = np.cross(v1, v2)
+        d = - np.dot(normal, p1)
+        group1 = []
+        group2 = []
+        for atom in cluster:
+            val = np.dot(normal, atom.position) + d
+            if val > 0:
+                group1.append(atom)
+            else:
+                group2.append(atom)
+        return group1, group2
---
+++ test.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pytest
+from ase import Atoms
+from Disturber import Disturber
+
+
+@pytest.fixture()
+def cluster_30():
+    positions = []
+    for i in range(30):
+        positions.append(np.random.rand(3))
+    cluster = Atoms('Fe' + str(30), positions=positions)
+    return cluster
+
+
+@pytest.fixture()
+def cluster_50():
+    positions = []
+    for i in range(50):
+        positions.append(np.random.rand(3))
+    cluster = Atoms('Fe' + str(50), positions=positions)
+    return cluster
+
+
+def test_cluster_split_30(cluster_30):
+    group1, group2 = Disturber.split_cluster(cluster_30)
+    assert len(group1) + len(group2) == 30
+    assert len(set(group1).intersection(set(group2))) == 0
+
+
+def test_cluster_split_50(cluster_50):
+    group1, group2 = Disturber.split_cluster(cluster_50)
+    assert len(group1) + len(group2) == 50
+    assert len(set(group1).intersection(set(group2))) == 0
