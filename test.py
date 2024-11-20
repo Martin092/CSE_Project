1d34fd07f0a23afd63c641a53ad043c5bf89a5b4
Mohammed Balfakeih <M.H.M.Balfakeih@student.tudelft.nl>
2024-11-20 17:19:04 +0100

    Added basic test for MD run

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 import numpy as np
 import pytest
 from ase import Atoms
+from ase.calculators.lj import LennardJones
 from Disturber import Disturber
 from GeneticAlgorithm import GeneticAlgorithm
 
+np.random.seed(0)
+
+@pytest.fixture()
+def cluster_2():
+    positions = []
+    for i in range(2):
+        positions.append(np.random.rand(3))
+    cluster = Atoms('Fe' + str(2), positions=positions)
+    cluster.calc = LennardJones()
+    return cluster
 
 @pytest.fixture()
 def cluster_30():
@@ -11,6 +22,7 @@ def cluster_30():
     for i in range(30):
         positions.append(np.random.rand(3))
     cluster = Atoms('Fe' + str(30), positions=positions)
+    cluster.calc = LennardJones()
     return cluster
 
 
@@ -20,6 +32,7 @@ def cluster_50():
     for i in range(50):
         positions.append(np.random.rand(3))
     cluster = Atoms('Fe' + str(50), positions=positions)
+    cluster.calc = LennardJones()
     return cluster
 
 @pytest.fixture()
@@ -32,6 +45,8 @@ def cluster_30_2():
     for i in range(30):
         positions2.append(np.random.rand(3))
     cluster2 = Atoms('Fe' + str(30), positions=positions2)
+    cluster1.calc = LennardJones()
+    cluster2.calc = LennardJones()
     return cluster1, cluster2
 
 
@@ -66,6 +81,7 @@ def test_crossover(cluster_30_2):
     a, b = GeneticAlgorithm.crossover(cluster_30_2[0], cluster_30_2[1])
     assert len(a) == len(b)
 
-def test_md(cluster_30):
-    Disturber.md(cluster_30, 100, 2, seed=100)
-    print(cluster_30.positions)
+def test_md(cluster_2):
+    Disturber.md(cluster_2, 100, 2, seed=0)
+    resulting_positions = np.array([[5665.16673931,420178.45149765,-62162.09537009],[-5664.07304262,-420177.31265348,62163.34402758]])
+    assert np.isclose(cluster_2.positions, resulting_positions).all()
