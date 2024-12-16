b78683cf8bc1da33a6e490b91991204c1d881161
Martin <martin.damianof@gmail.com>
2024-12-17 00:52:17 +0100

    script for transfering stuff to delftblue

---
+++ scripts/transfer-to-delftblue.sh
@@ -0,0 +1,11 @@
+echo "$PWD"
+
+mkdir temp
+cp -r src temp
+cp -r auxiliary temp
+cp requirements.txt temp
+
+read -p "Enter netid pls: " name
+scp -pr temp "$name"@login.delftblue.tudelft.nl:/home/"$name"/Project
+
+rm -r temp
